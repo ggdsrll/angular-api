e6077cf1994b9bbbb258130d67bbc89b134616ef
ggdsrll <148755800+ggdsrll@users.noreply.github.com>
2025-01-21 09:51:21 +0100

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  posts: any[] = [];
+  constructor(private servicio:DataService){}//inyectamos el servicioS - inyecion de dependencias
+  ngOnInit(){
+    this.servicio.getPosts().subscribe((data:any[])=>{
+      this.posts = data;
+      console.log(this.posts);  
+    });
+  }
+}
